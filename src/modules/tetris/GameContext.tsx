e04b9e35fc4548f7113c8b6ed1dd27e99dbfa4f6
Bas Kaal <3askaal@gmail.com>
2022-08-04 22:46:13 +0200

    modularize and refactor tetris specific logic

--- src/modules/tetris/GameContext.tsx
+++ src/modules/tetris/GameContext.tsx
@@ -3,6 +3,7 @@ import ReactGA4 from 'react-ga4'
 import { groupBy, includes, sum } from 'lodash'
 import { Block, generateShape, Shape } from '../helpers/generate';
 import { useInterval } from '../helpers/interval';
+import { checkBlocks, moveX, moveY, rotate } from './actions';
 
 export interface Dimensions {
   width: number;
@@ -11,7 +12,7 @@ export interface Dimensions {
 
 export interface Score {
   level: number;
-  score: number;
+  points: number;
   rows: number;
 }
 
@@ -38,7 +39,7 @@ export const GameContextDefaults = {
   blocks: [],
   setBlocks: () => {},
   dimensions: { height: 36, width: 20 },
-  score: { level: 1, score: 0, rows: 0 },
+  score: { level: 1, points: 0, rows: 0 },
   gameOver: false,
   gamePaused: false,
   setGamePaused: () => {},
@@ -60,12 +61,12 @@ export const GameProvider = ({ children }: any) => {
   const [dimensions] = useState({ height: 36, width: 20 })
   const [gameOver, setGameOver] = useState(false)
   const [gamePaused, setGamePaused] = useState(false)
-  const [score, setScore] = useState({ level: 1, score: 0, rows: 0 })
+  const [score, setScore] = useState<Score>({ level: 1, points: 0, rows: 0 })
 
-  const onStartGame = (initialShape = generateShape(dimensions)) => {
+  const start = (initialShape = generateShape(dimensions)) => {
     gameHasStarted.current = true;
     setGameOver(false)
-    setScore({ level: 1, score: 0, rows: 0 })
+    setScore({ level: 1, points: 0, rows: 0 })
     setBlocks([])
     setShape(initialShape)
 
@@ -85,161 +86,55 @@ export const GameProvider = ({ children }: any) => {
     setBlocksState(blocks)
   }
 
-  const checkShapePosition = (nextShape: Shape) => {
-    const hitsBlock = blocksRef.current.some((block: Block) =>
-      nextShape.blocks.some((nextBlock) =>
-        (nextShape.x + nextBlock.x) === block.x && (nextShape.y + nextBlock.y) === block.y
-      )
-    );
-
-    const hitsBottom = (nextShape.y + nextShape.height) >= dimensions.height + 1;
-    const hitsSide = (nextShape.x < 0) || (nextShape.x + nextShape.width > dimensions.width);
-
-    return hitsBlock || hitsBottom || hitsSide
+  const onMoveX = (direction: 'left' | 'right') => {
+    const nextShape = moveX(shapeRef.current, blocksRef.current, dimensions, direction);
+    if (nextShape) setShape(nextShape)
   }
 
-  const moveX = (direction: 'left' | 'right') => {
-    const movements =  {
-      left: -1,
-      right: 1
-    }
-
-    const nextShape = {
-      ...shapeRef.current,
-      x: shapeRef.current?.x + movements[direction]
-    }
-
-    const isHit = checkShapePosition(nextShape)
-
-    if (isHit) {
-      return
-    }
-
-    setShape(nextShape)
-  }
-
-  const moveY = async (shouldUpdateState: boolean = true) => {
+  const onMoveY = async (shouldUpdateState: boolean = true) => {
     if (!gameHasStarted.current) throw new Error('Game has not been started yet');
     if (!shapeRef.current) throw new Error('Could not find active shape');
 
-    const nextShape: Shape = { ...shapeRef.current, y: shapeRef.current.y + 1 }
-    const isHit = checkShapePosition(nextShape)
-    const isGameOver = isHit && shapeRef.current.y === 2
+    const [newShape, newBlocks, newGameOver] = await moveY(shapeRef.current, blocksRef.current, dimensions, shouldUpdateState)
 
-    if (isHit) {
-      setBlocks([
-        ...blocksRef.current,
-        ...shapeRef.current.blocks.map((currentBlock: any) => ({
-          ...currentBlock,
-          x: shapeRef.current.x + currentBlock.x,
-          y: shapeRef.current.y + currentBlock.y,
-          color: shapeRef.current.color
-        })),
-      ])
-
-      setShape(generateShape(dimensions))
-    } else {
-      if (shouldUpdateState) {
-        setShape(nextShape)
-      } else {
-        shapeRef.current = nextShape
-      }
-    }
-
-    if (isGameOver) {
-      setShape(null)
-      setGameOver(true)
-    }
-
-    return isHit
+    if (newBlocks) setBlocks(newBlocks)
+    if (newShape) setShape(newShape)
+    if (newGameOver) setGameOver(newGameOver)
   }
 
-  const drop = async () => {
+  const onDrop = async () => {
+    const initialBlocksLength = blocksRef.current.length
     let isHit = false
 
     while (!isHit) {
-      isHit = await moveY(false)
-    }
-  }
+      const [newShape, newBlocks, newGameOver] = await moveY(shapeRef.current, blocksRef.current, dimensions)
+      isHit = (newBlocks?.length || 0) > initialBlocksLength
 
-  const rotate = () => {
-    const rotatedShape = {
-      ...shapeRef.current,
-      width: shapeRef.current.height,
-      height: shapeRef.current.width,
-    }
-
-    rotatedShape.blocks = shapeRef.current.blocks.map((block: Block) => ({
-      x: (rotatedShape.width - 1) - block.y,
-      y: block.x
-    }))
-
-    if (rotatedShape.width > shapeRef.current.width) {
-      if (rotatedShape.x + rotatedShape.width > dimensions.width) {
-        rotatedShape.x -= (rotatedShape.width - shapeRef.current.width)
+      if (isHit) {
+        if (newBlocks) setBlocks(newBlocks)
+        if (newShape) setShape(newShape)
+        if (newGameOver) setGameOver(newGameOver)
       }
     }
-
-    const isHit = checkShapePosition(rotatedShape)
-
-    if (isHit) {
-      return
-    }
-
-    setShape(rotatedShape)
   }
 
-  const getFullRows = (): number[] => {
-    const inactiveRows = groupBy(blocksRef.current, 'y')
+  const onRotate = () => {
+    const rotatedShape = rotate(shapeRef.current, blocksRef.current, dimensions)
 
-    const fullRows = Object.entries(inactiveRows)
-      .filter(([index, inactiveRow]) => inactiveRow.length > dimensions.width - 1 && index)
-      .map(([index]) => Number(index))
-
-    return fullRows
+    if (rotatedShape) setShape(rotatedShape)
   }
 
-  const checkBlocks = () => {
-    const fullRows = getFullRows()
-
-    if (!fullRows.length) {
-      return
-    }
-
-    const pointsForAmountRows = [40, 100, 300, 1200]
-    const amountRowsIndex = fullRows.length - 1
-
-    const newRows = score.rows + fullRows.length
-    const newLevel = Math.floor(newRows / 10) + 1
-    const newScore = score.score + (pointsForAmountRows[amountRowsIndex] * score.level)
-
-    setScore({
-      level: newLevel,
-      score: newScore,
-      rows: newRows,
-    })
-
-    setBlocks(blocksRef.current.map((currentBlock: Block) => ({
-      ...currentBlock,
-      dead: includes(fullRows, currentBlock.y)
-    })))
-
-    setTimeout(() => {
-      setBlocks(
-        blocksRef.current
-          .filter(({ dead }: Block) => !dead)
-          .map((block: Block) => ({
-            ...block,
-            y: block.y + sum(
-              fullRows.map((rowY) => block.y < rowY ? 1 : 0)
-            )
-          }))
-      )
-    }, 500)
-  }
 
   useEffect(() => {
-    checkBlocks()
+    const res = checkBlocks(blocksRef.current, score, dimensions)
+
+    if (res) {
+      const [newScore, deadBlocks, newBlocks] = res
+
+      if (newScore) setScore(newScore)
+      if (deadBlocks) setBlocks(deadBlocks)
+      if (newBlocks) setTimeout(() => setBlocks(newBlocks), 500)
+    }
   }, [blocks.length])
 
   useEffect(() => {
