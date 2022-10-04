cb77bdc86b70ceb3f3eaf5ffc9cc2f806378ba4d
Bas Kaal <3askaal@gmail.com>
2022-10-04 14:05:20 +0200

    fix perfomance issue

--- src/index.tsx
+++ src/index.tsx
@@ -8,15 +8,7 @@ import './fonts.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 
-root.render(
-  process?.env?.NODE_ENV !== 'development' ? (
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  ) : (
-    <App />
-  )
-)
+root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
