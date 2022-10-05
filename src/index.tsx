1c0bf7b35aa2ee8e94185f7ee0c5e44c69b8d240
Bas Kaal <3askaal@gmail.com>
2022-10-05 18:23:25 +0200

    add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 
 import 'reset-css/reset.css'
 import './fonts.css'
+
+Sentry.init({
+  dsn: "https://c36da8fd4e2a4b3184a11081b1f10022@o107915.ingest.sentry.io/4503929981304832",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 
