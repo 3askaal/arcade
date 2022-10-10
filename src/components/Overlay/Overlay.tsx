6e35dc20e9ce4b0a24d468b50380145ff9a738ec
Bas Kaal <3askaal@gmail.com>
2022-10-10 21:17:29 +0200

    create custom hook for menu select, improve game over view, refactor

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -1,15 +1,15 @@
 import { s, rgba } from '3oilerplate'
 
 export const Overlay = s.div(({ theme }: any) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'space-between',
   position: 'absolute',
   top: 0,
   left: 0,
   right: 0,
   bottom: 0,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'space-between',
   p: 'l',
   backgroundColor: rgba(theme.colors.grey90, .96),
   zIndex: 1000
